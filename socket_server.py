08f8cd7f11173e637cb587793826b13103938d70
z-kidy <zjindiss@gmail.com>
2015-05-28 19:27:18 +0800

    add thread

--- socket_server.py
+++ socket_server.py
@@ -1,9 +1,10 @@
 import socket
 import sys
 from pymouse import PyMouse
+import thread
 
 HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 8789 # Arbitrary non-privileged port
+PORT = 8885 # Arbitrary non-privileged port
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
@@ -18,25 +19,30 @@ print 'Socket bind complete'
  
 s.listen(10)
 print 'Socket now listening'
- 
-conn, addr = s.accept()
-print 'Connected with ' + addr[0] + ':' + str(addr[1])
-#now keep talking with the client
-m = PyMouse()
 
-while 1:
-    #wait to accept a connection - blocking call   
-    data = conn.recv(1024)
-    reply = 'OK...' + data
-    data = data.split(',')
-    offset_x = int(data[0])
-    offset_y = int(data[1])
-    print str(offset_x) + str(offset_y)
-    x = m.position()[0]
-    y = m.position()[1]
-    m.move(x + offset_x , y + offset_y)
-     
-    conn.sendall(reply)
+def pymouse_action(conn):        
+    while 1:
+        #wait to accept a connection - blocking call   
+        data = conn.recv(1024)
+        reply = 'OK...' + data
+        data = data.split(',')
+        offset_x = int(data[0])
+        offset_y = int(data[1])
+        print "move "+str(offset_x) + "," + str(offset_y)
+        m = PyMouse()
+        x = m.position()[0]
+        y = m.position()[1]
+        m.move(x + offset_x , y + offset_y)
+         
+        conn.sendall(reply)
  
-conn.close()
+    conn.close()
+
+while 1: 
+    conn, addr = s.accept()
+    print 'A New Connected with ' + addr[0] + ':' + str(addr[1])
+    #now keep talking with the client
+    mouse = thread.start_new_thread(pymouse_action, (conn,) )
+
+
 s.close()
